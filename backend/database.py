5b2ce9288031e484fa7638f234571d6576efce9d
thuan04 <dpt12032004@gmail.com>
2026-09-09 23:09:07 +0700

    Update database connection to Azure SQL

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = os.environ.get(
     "DATABASE_URL",
-    r"mssql+pymssql://shophub:gaugau%4012@shophub.database.windows.net:1433/hubroom?encrypt=yes"
+    r"mssql+pymssql://cloud:123Gaugau@roomhubshop.database.windows.net:1433/roomhubshop?charset=utf8"
 )
 
 engine = create_engine(DATABASE_URL)
